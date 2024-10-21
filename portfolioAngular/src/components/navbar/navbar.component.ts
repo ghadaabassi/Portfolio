74627fea70fc2ad0b9676f18ec530ca748c03f30
ghada <gh.abassi01@gmail.com>
2024-10-21 22:14:35 +0100

    AddDaekMode+MajorDesignChanges

--- portfolioAngular/src/components/navbar/navbar.component.ts
+++ portfolioAngular/src/components/navbar/navbar.component.ts
@@ -1,9 +1,27 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  darkMode = false;
+
+  toggleTheme() {
+    this.darkMode = !this.darkMode;
+    const element = document.querySelector('body');
+    if (element) {
+      if (this.darkMode) {
+        console.log('switchdark');
+        element.classList.add('dark-mode');
+      } else {
+        console.log('switchLight');
+        element.classList.remove('dark-mode');
+      }
+    }
+  }
+}
